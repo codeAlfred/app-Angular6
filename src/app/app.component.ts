727d2c7b5b9adb1e16b3376e7576a6a7462e4326
codeAlfred <munozrodas.luisalfredo@gmail.com>
2019-04-03 22:56:45 -0500

    create ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appCrud';
+
+  title: string = 'appCrud';
+  //arreglo de objetos de empleados
+  employees=[
+    {name: "luis" , position: "ceo"},
+    {name: "alfredo" , position: "cto"},
+    {name: "jaziel" , position: "coo"}
+  ];
+  /*para que el doble enlace de datos funcione, hay que ejecutar un modelo.
+   */
+  model: any ={};
+
+  //metodos para CRUD
+  //create
+  addEmployee():void {
+    this.employees.push(this.model);
+  }
+  //delete
+  deleteEmployee():void {
+
+  }
+  //prepara para editar
+  editEmployee():void {
+
+  }
+  //update
+  updateEmployee():void {
+
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// importar el mudulo de Formulario para angular
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
