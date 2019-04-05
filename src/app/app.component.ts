5d72ae4c11350eec95f25ee3ec0d71638675c877
codeAlfred <munozrodas.luisalfredo@gmail.com>
2019-04-04 23:05:24 -0500

    editar ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,15 @@ export class AppComponent {
   title: string = 'appCrud';
   //arreglo de objetos de empleados
   employees=[
-    {name: "luis" , position: "ceo"},
-    {name: "alfredo" , position: "cto"},
-    {name: "jaziel" , position: "coo"}
+    {name: "luis" , position: "ceo", email: "email.@gamil.com"},
+    {name: "alfredo" , position: "cto", email: "email2.@gamil.com"},
+    {name: "jaziel" , position: "coo", email: "email3.@gamil.com"}
   ];
   /*para que el doble enlace de datos funcione, hay que ejecutar un modelo.
    */
   model: any ={};
+  //segundo modelo para el updateEmployee
+  model2: any ={};
 
   //metodos para CRUD
   //create
@@ -28,12 +30,22 @@ export class AppComponent {
 
   }
   //prepara para editar
-  editEmployee():void {
-
+  myValue;
+  editEmployee(i):void {
+    this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+    this.model2.email = this.employees[i].email;
+    this.myValue = i;
   }
   //update
   updateEmployee():void {
-
+    let i = this.myValue;
+    for(let j=0; j<this.employees.length; j++){
+      if(i == j){
+        this.employees[i]= this.model2;
+        this.model2={};
+      }
+    }
   }
 
 
